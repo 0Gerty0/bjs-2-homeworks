76c38b3e7de46300844fd066ec4c66691565c349
Aleks Danilushkin <thenumber@ya.ru>
2025-02-27 20:31:40 +0300

    Base concepts done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,33 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+
+  let d = b ** 2 - 4 * a * c;
+  if (d<0) {
+      return [];
+  }
+  else if (d === 0) {
+      return [-b / (2 * a)]
+  }
+
+  else {
+      return [
+          (-b + Math.sqrt(d)) / (2 * a),
+          (-b - Math.sqrt(d)) / (2 * a)
+      ];
+  }
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+function calculateTotalMortgage (percent, contribution, amount, countMonth)  {
+
+  let monthlyRate = (percent / 100) / 12;
+
+  let loanBody = amount - contribution;
+
+  if (loanBody <= 0) {
+      return 0;
+  }
+  let monthlyPayment = loanBody * (monthlyRate + (monthlyRate/((1 + monthlyRate)**countMonth - 1)));
+  let totalAmount = monthlyPayment * countMonth;
+  return Number(totalAmount.toFixed(2));
 }
