7e413275cf47f77b4d0ed7e6370b1d4b78640c51
Aleks Danilushkin <thenumber@ya.ru>
2025-07-02 20:05:46 +0300

    Arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
+/*Задача 1. Сравнение массивов*/
 function compareArrays(arr1, arr2) {
-  
+  return (
+    arr1.length === arr2.length &&
+    arr1.every((value, index) => value === arr2[index])
+  );
 }
 
+/*Задача 2. Средний возраст пользователей одного пола*/
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const filtered = users.filter(user => user.gender === gender);
+
+  if (filtered.length === 0) {
+    return 0;
+  }
+
+  const sumAges = filtered.reduce((sum, user) => sum + user.age, 0);
+  return sumAges / filtered.length;
 }
